668db5877a89602b86bc2be4e1a38ff8d237dd98
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-02-17 12:11:28 -0500

    assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :campers, except: [:update, :destroy]
+  resources :signups, only: [:create]
+  resources :activities, only: [:index, :destroy]
 end
